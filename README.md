82c618b592a2b759ba851d0bca44ca51b492683d
Alexis Tan <alexis@sen-dev.com>
2018-06-14 17:15:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+AutoSenither
+===========
+
+AutoSenither is a Discord bot built using [JDA](https://github.com/DV8FromTheWorld/JDA), more information coming soon...
---
+++ src/main/java/com/avairebot/senither/AutoSenither.java
@@ -0,0 +1,8 @@
+package com.avairebot.senither;
+
+public class AutoSenither {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
