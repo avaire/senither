e20ea00336508c9f05ba5ccfb02ffbdbe4581099
Alexis Tan <alexis@sen-dev.com>
2019-07-07 17:05:34 +0200

    Add bot porn links to the advertisement block list

--- src/main/java/com/avairebot/senither/utils/ChatFilterUtil.java
+++ src/main/java/com/avairebot/senither/utils/ChatFilterUtil.java
@@ -10,12 +10,27 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ChatFilterUtil {
+
+    private static final List<String> blockedDomains = Arrays.asList(
+        "discordapp.com/invite/",
+        "discord.gg/"
+    );
+
+    private static final List<String> blockedMentionedDomains = Arrays.asList(
+        // Porn Advertisements
+        "viewc.site",
+        "privatepage.vip",
+        "nakedphotos.club"
+    );
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ChatFilterUtil.class);
     private static final int MAX_REDIRECTS = 10;
@@ -26,6 +41,8 @@ public class ChatFilterUtil {
             public String load(@Nonnull String key) throws Exception {
                 try {
                     return getTrueUrl(key, 0);
+                } catch (MalformedURLException ignored) {
+                    return key;
                 } catch (IOException e) {
                     LOGGER.error("An exception was thrown while trying to resolve the true url for {}", key, e);
                     return key;
@@ -34,7 +51,7 @@ public class ChatFilterUtil {
         });
 
     private static final Pattern urlPattern = Pattern.compile(
-        "(?:^|[\\W])((ht|f)tp(s?):\\/\\/|www\\.)"
+        "(?:^|[\\W])((ht|f)tp(s?):\\/\\/|www\\.|)"
             + "(([\\w\\-]+\\.){1,}?([\\w\\-.~]+\\/?)*"
             + "[\\p{Alnum}.,%_=?&#\\-+()\\[\\]\\*$~@!:/{};']*)",
         Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL
@@ -42,15 +59,24 @@ public class ChatFilterUtil {
 
     public static boolean isAdvertisement(Message jdaMessage) {
         String message = jdaMessage.getContentRaw();
-        Matcher matcher = urlPattern.matcher(message);
 
-        while (matcher.find()) {
-            String url = CacheUtil.getUncheckedUnwrapped(cache, message.substring(matcher.start(1), matcher.end()));
-
-            if (url.contains("discordapp.com/invite/")) {
+        for (String domain : blockedMentionedDomains) {
+            if (message.toLowerCase().contains(domain)) {
                 return true;
             }
         }
+
+        Matcher matcher = urlPattern.matcher(message);
+        while (matcher.find()) {
+            String url = CacheUtil.getUncheckedUnwrapped(cache, message.substring(matcher.start(1), matcher.end()));
+
+            for (String domain : blockedDomains) {
+                if (url.contains(domain)) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
@@ -59,6 +85,8 @@ public class ChatFilterUtil {
         if (redirectsAttempts > MAX_REDIRECTS) {
             return url;
         }
+
+        System.setProperty("http.agent", "Chrome");
 
         HttpURLConnection con = (HttpURLConnection) (new URL(url).openConnection());
         con.setRequestMethod("GET");
